03efd29078e88e78c112519ac909b4fdac5adc89
이승준 <nate199458@gmail.com>
2021-05-09 21:54:51 +0900

    init

---
+++ develop/intro.py
@@ -0,0 +1,74 @@
+import sys
+# from PyQt5 import QtWidgets
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from pixby.newSR import Create_SR_Model
+from pixby.compare import compareModel, resultModel
+# form_class = uic.loadUiType("intro.ui")[0]
+
+
+class WindowClass(QMainWindow):
+    def __init__(self):
+        super(WindowClass, self).__init__()
+        loadUi('./pixby/ui/intro.ui', self)
+
+        self.startBtn.clicked.connect(self.gotoChoice)
+        self.compare.clicked.connect(self.gotoCompare)
+
+    def gotoChoice(self):
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def gotoCompare(self):
+        widget.setCurrentIndex(widget.currentIndex()+4)
+
+
+class Choice(QMainWindow):
+    def __init__(self):
+        super(Choice, self).__init__()
+        loadUi('./pixby/ui/choice.ui', self)
+
+        self.gotoModelBtn.clicked.connect(self.gotoModel)
+        self.goToCreateSR.clicked.connect(self.gotoCreateSR)
+    def gotoModel(self):
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def gotoCreateSR(self):
+        widget.setCurrentIndex(widget.currentIndex()+2)
+        
+
+class Model(QMainWindow):
+    def __init__(self):
+        super(Model, self).__init__()
+        loadUi('./pixby/ui/model.ui', self)
+
+        self.modelBtn.clicked.connect(self.openModel)
+        self.imageBtn.clicked.connect(self.openImage)
+
+    def openModel(self):
+        modelOpen = QFileDialog.getOpenFileName(self, 'open file', './')
+
+    def openImage(self):
+        imageOpen = QFileDialog.getOpenFileName(self, 'open file', './')
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    widget = QStackedWidget()
+    windowclass = WindowClass()
+    choice = Choice()
+    model = Model()
+    create_sr = Create_SR_Model()
+    compare_model = compareModel()
+    result_model = resultModel()
+    widget.addWidget(windowclass)
+    widget.addWidget(choice)
+    widget.addWidget(model)
+    widget.addWidget(create_sr)
+    widget.addWidget(compare_model)
+    widget.addWidget(result_model)    
+    widget.setFixedHeight(960)
+    widget.setFixedWidth(1280)
+    widget.show()
+    app.exec_()
---
+++ develop/pixby/compare.py
@@ -0,0 +1,57 @@
+import sys, os
+from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5 import uic
+
+#UI파일 연결
+#단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+ 
+# 1. ui 연결 
+# 연결할 ui 파일의 경로 설정
+form1 = resource_path('ui/compare.ui')
+form2 = resource_path('ui/res.ui')
+# ui 로드 
+form_class1 = uic.loadUiType(form1)[0]
+form_class2 = uic.loadUiType(form2)[0]
+
+#화면을 띄우는데 사용되는 Class 선언
+class compareModel(QMainWindow, form_class1):
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        self.setStyleSheet("background-color: #777777;")
+        self.comeImage.resize(250, 250)
+        self.selectModel1.clicked.connect(self.fileopen)
+        self.selectModel2.clicked.connect(self.fileopen)
+        # 이미지 보여주기
+        self.selectImage.clicked.connect(self.putImage)
+        self.nextButton.clicked.connect(self.nextPage)
+
+    def fileopen(self):
+        global filename
+        print('aa')
+        filename = QFileDialog.getOpenFileName(self, 'Open File') 
+        print(filename)
+
+    def putImage(self):
+        global compare_image
+        # 이미지 주소 저장
+        compare_image = QFileDialog.getOpenFileName(self, 'Open File')
+        # 이미지를 라벨에 붙여서 뛰우기
+        self.comeImage.setPixmap(QtGui.QPixmap(compare_image[0]))
+
+    def nextPage(self):
+        resWindow = resultModel()
+
+
+class resultModel(QMainWindow, form_class2):
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+
