c76b500cd13b487069d963bd43f53c7e16740ee6
이승준 <nate199458@gmail.com>
2021-05-16 18:02:01 +0900

    compare
    Former-commit-id: 0d58885d5e64a21d7a2495961294b2ef686f2f4a

--- develop/intro.py
+++ develop/intro.py
@@ -1,4 +1,6 @@
 import sys, os
+import pixby.cnn.inferencing as Inf # cnn 추론 
+import pixby.cnn.trainer as cnn_trainer
 # from PyQt5 import QtWidgets
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import *
@@ -70,13 +72,106 @@ class Choice(QMainWindow):
         widget.setCurrentIndex(widget.currentIndex()+2)
 
 
-compare_ui = resource_path('pixby/ui/compare.ui')
-res_ui = resource_path('pixby/ui/res.ui')
 # ui 로드 
-compare_form = uic.loadUiType(compare_ui)[0]
+new_cnn_ui = resource_path('pixby/ui/newCNN.ui')
+new_cnn_form = uic.loadUiType(new_cnn_ui)[0]
+class Create_CNN_Model(QMainWindow, new_cnn_form):
+    send_valve_popup_signal = pyqtSignal(bool, name='sendValvePopupSignal')
 
+    # 인자 값
+    tr_path = ""
+    te_path = ""
+    sa_path = ""
+    name = ""
+    learning_rate=""
+    epoch = ""
+    batch = 32
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+        self.save_btn.clicked.connect(self.save)
+        self.train_btn.clicked.connect(self.train_folder)
+        self.test_btn.clicked.connect(self.test_folder)
+        self.start_button.clicked.connect(self.goToCNN)
+
+        self.size32.setChecked(True)
+        self.size16.clicked.connect(self.radioButtonClicked)
+        self.size32.clicked.connect(self.radioButtonClicked)
+        self.size64.clicked.connect(self.radioButtonClicked)
+
+    def radioButtonClicked(self):
+        if self.size16.isChecked():
+            Create_CNN_Model.batch = 16
+        elif self.size32.isChecked():
+            Create_CNN_Model.batch = 32
+        elif self.size64.isChecked():
+            Create_CNN_Model.batch = 64
+        print(Create_CNN_Model.batch)
+
+
+    def warningMSG(self, title: str, content: str):
+        msg = QMessageBox()
+        msg.setWindowTitle(title)
+        msg.setText(content)
+        msg.setStandardButtons(QMessageBox.Ok)
+        result = msg.exec_()
+        if result == QMessageBox.Ok:
+            self.send_valve_popup_signal.emit(True)
+
+    def train_folder(self):
+        # global working_path2
+        options = QFileDialog.Options()
+        options |= QFileDialog.ShowDirsOnly
+        Create_CNN_Model.tr_path = QFileDialog.getExistingDirectory(self,"select Directory")
+        self.train_path.clear()
+        self.train_path.append('경로: {}'.format(Create_CNN_Model.tr_path))
+  
+    def test_folder(self):
+        # global working_path2
+        options = QFileDialog.Options()
+        options |= QFileDialog.ShowDirsOnly
+        Create_CNN_Model.te_path = QFileDialog.getExistingDirectory(self,"select Directory")
+        self.test_path.clear()
+        self.test_path.append('경로: {}'.format(Create_CNN_Model.te_path))
+
+    # model 1
+    def save(self):
+        try:
+            options = QFileDialog.Options()
+            options |= QFileDialog.ShowDirsOnly
+            Create_CNN_Model.sa_path = QFileDialog.getExistingDirectory(self,"select Directory")
+            self.save_path.clear()
+            self.save_path.append('경로: {}'.format(Create_CNN_Model.sa_path))
+        except:
+            self.warningMSG("주의", "경로 설정을 다시해주세요.")
+
+    def goToCNN(self):
+        Create_CNN_Model.learning_rate = self.learning_rate.toPlainText()
+        Create_CNN_Model.epoch = self.epoch.toPlainText()
+        Create_CNN_Model.name = self.name.toPlainText()
+        Create_CNN_Model.learning_rate = float(Create_CNN_Model.learning_rate)
+        # print(Create_CNN_Model.learning_rate)
+        print("1")
+        Create_CNN_Model.epoch = int(Create_CNN_Model.epoch)
+        print("2")
+        if Create_CNN_Model.learning_rate >=0.1:
+            self.warningMSG("주의", "learning rate는 0.1보다 작게 해주셔야 됩니다.")
+        elif Create_CNN_Model.sa_path and Create_CNN_Model.tr_path and Create_CNN_Model.te_path:
+            print(Create_CNN_Model.tr_path, Create_CNN_Model.te_path,  Create_CNN_Model.sa_path, Create_CNN_Model.name ,Create_CNN_Model.learning_rate, Create_CNN_Model.epoch, Create_CNN_Model.batch)
+            cnn_trainer.CNN_Train(Create_CNN_Model.tr_path, Create_CNN_Model.te_path,  Create_CNN_Model.sa_path, Create_CNN_Model.name ,Create_CNN_Model.learning_rate, Create_CNN_Model.epoch, Create_CNN_Model.batch)
+        else:
+            self.warningMSG("주의", "이미지와 모델을 먼저 집어넣어주세요.")
+        # except:
+            # self.warningMSG("주의", "LEARNING RATE, epoch을 숫자로 입력해주셔야 합니다.")
+
+        
+
+
+compare_ui = resource_path('pixby/ui/compare.ui')
+compare_form = uic.loadUiType(compare_ui)[0]
 #화면을 띄우는데 사용되는 Class 선언
-class compareModel(QMainWindow, compare_form ):
+class Compare_Model(QMainWindow, compare_form ):
     command = QtCore.pyqtSignal(str) # 이미지 주소 전달
     model_1= ""
     model_2 = ""
@@ -102,6 +197,8 @@ class compareModel(QMainWindow, compare_form ):
         backbutton.setStyleSheet('image:url(img/undo.png);border:0px;background-color:#F2F2F2')
         backbutton.clicked.connect(self.goToBack)
     
+
+    
         self.selectModel1.clicked.connect(self.choiceModel_1)
         self.selectModel2.clicked.connect(self.choiceModel_2)
         # 이미지 보여주기
@@ -114,8 +211,9 @@ class compareModel(QMainWindow, compare_form ):
 
     # 뒤로가기 -> classfication 설정 페이지
     def goToBack(self):
-        widget.setCurrentIndex(widget.currentIndex()-4)
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
+        # widget.setCurrentIndex(widget.currentIndex()-4)
     #경고 창 메서드...
     def warningMSG(self, title: str, content: str):
         msg = QMessageBox()
@@ -133,25 +231,25 @@ class compareModel(QMainWindow, compare_form ):
         # 폴더 구조 선택
         options = QFileDialog.Options()
         options |= QFileDialog.ShowDirsOnly
-        compareModel.working_path1 = QFileDialog.getExistingDirectory(self,"select Directory")
+        Compare_Model.working_path1 = QFileDialog.getExistingDirectory(self,"select Directory")
         # 비우고 경로 입력
         self.dir1.clear()
-        self.dir1.append('경로: {}'.format(compareModel.working_path1))
+        self.dir1.append('경로: {}'.format(Compare_Model.working_path1))
 
 
     def folder_second(self):
         # global working_path2
         options = QFileDialog.Options()
         options |= QFileDialog.ShowDirsOnly
-        compareModel.working_path2 = QFileDialog.getExistingDirectory(self,"select Directory")
+        Compare_Model.working_path2 = QFileDialog.getExistingDirectory(self,"select Directory")
         self.dir2.clear()
-        self.dir2.append('경로: {}'.format(compareModel.working_path2))
+        self.dir2.append('경로: {}'.format(Compare_Model.working_path2))
 
     # model 1
     def choiceModel_1(self):
         try:
             name = QFileDialog.getOpenFileName(self, 'Open File')[0]
-            compareModel.model_1 = name # 분류모델 경로
+            Compare_Model.model_1 = name # 분류모델 경로
             self.model_name1.clear()
             self.model_name1.append(name.split('/')[-1])
         except:
@@ -159,15 +257,17 @@ class compareModel(QMainWindow, compare_form ):
     # model 2 
     def choiceModel_2(self):
         name = QFileDialog.getOpenFileName(self, 'Open File')[0]
-        compareModel.model_2 = name
+        Compare_Model.model_2 = name
 
         # 모델 경로 보여주기
         self.model_name2.clear()
         self.model_name2.append(name.split('/')[-1])
 
     def nextPage(self):
-        if compareModel.working_path1 and compareModel.working_path2:
-            resultModel(self)
+        if Compare_Model.working_path1 and Compare_Model.working_path2:
+            Inf.Infer(Compare_Model.working_path1, Compare_Model.model_1)
+            Inf.Infer(Compare_Model.working_path2, Compare_Model.model_2)
+            Result_Model(self)
         else:
             self.warningMSG("주의", "이미지와 모델을 먼저 집어넣어주세요.")
 
@@ -314,11 +414,11 @@ class Learn_SR_Model(QMainWindow, learn_ui_form) :
 
 
 
-
+res_ui = resource_path('pixby/ui/res.ui')
 res_form = uic.loadUiType(res_ui)[0]
-class resultModel(QMainWindow,res_form):
+class Result_Model(QMainWindow,res_form):
     def __init__(self, parent):
-        super(resultModel,self).__init__(parent)
+        super(Result_Model,self).__init__(parent)
         self.setupUi(self) # for_class2 ui 셋
         # UI 
         print(parent.working_path1)
@@ -342,13 +442,15 @@ if __name__ == "__main__":
     select_sr_model = Select_SR_Model()
     create_sr = Create_SR_Model()
     learn_sr = Learn_SR_Model()
-    compare_model = compareModel()
+    compare_model = Compare_Model()
+    create_cnn = Create_CNN_Model()
     widget.addWidget(windowclass)
     widget.addWidget(choice)
     widget.addWidget(select_sr_model)
     widget.addWidget(create_sr)
     widget.addWidget(learn_sr)
     widget.addWidget(compare_model)
+    widget.addWidget(create_cnn)
     widget.setFixedHeight(960)
     widget.setFixedWidth(1280)
     widget.show()
